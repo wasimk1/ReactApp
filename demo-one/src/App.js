a6a85a43c73f5ef4d2dbb393f978905a49765eb7
Wasim Khan <wasimkhan0816@gmail.com>
2024-05-28 12:31:59 +0530

    First React application

---
+++ demo-one/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://linkedin.com/in/wasimk1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Wasim Khan - LinkedIn
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
